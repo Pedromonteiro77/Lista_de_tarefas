8ebfc12b765cd4508019263c2a0974a71371645f
Pedromonteiro77 <107282276+Pedromonteiro77@users.noreply.github.com>
2023-09-01 22:39:48 -0300

    Lista de tarefas funcionau mais ainda não finalizada

---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void addTask(std::vector<std::string> & task, std::string & text, char & opc) {
+    while(true) {
+        std::cout << "Digite uma tarefa para a lista: ";
+        std::getline(std::cin, text);
+
+        if(text.empty()) {
+            std::cerr << "Lamento mais não posso adicionar uma tarefa vazia!" << "\n";
+            std::cout << "Aperte Enter para continuar...";
+            std::cin.get();
+            std::system("cls");
+            continue;
+        }
+        else {
+            task.push_back(text);
+            std::cout << "Deseja adicionar outra tarefa? (s)/(n): ";
+            std::cin >> opc;
+            std::cin.ignore();
+
+            if(opc == 's' || opc == 'S') { 
+                continue;
+            }
+            else {
+                std::system("cls");
+                break;
+            }
+        }
+    }
+
+    std::cout << "===== Lista de Tarefas =====" << std::endl;
+
+    for(auto i = 0; i < task.size(); i++) {
+        std::cout << (i + 1) << " - " << task[i] << std::endl;
+    }
+}
+
+void removeTask(std::vector<std::string> & task, std::string & text, char & opc, int & indice) {
+    std::cout << "Deseja remover alguma das tarefas? (s)/(n): ";
+    std::cin >> opc;
+    std::cin.ignore();
+
+    if(opc == 's' || opc == 'S') {
+        while(true) {
+            std::cout << "Digite o numero da tarefa que deseja remover: ";
+            std::cin >> indice;
+
+            if(indice >= 1 && indice <= task.size()) {
+                task.erase(task.begin() + (indice - 1));
+                std::system("cls");
+                std::cout << "Tarefa excluida com sucesso!" << std::endl;
+            }
+            else {
+                std::cout << "Numero INVALIDO! digite o numero de uma tarefa valido.";
+                continue;
+            }              
+        
+            std::cout << "===== Lista de Tarefas =====" << std::endl;
+
+            for(auto i = 0; i < task.size(); i++) {
+                std::cout << (i + 1) << " - " << task[i] << std::endl;
+            }
+
+            std::cout << "Deseja Remover mais uma tarefa? (s)/(n): ";
+            std::cin >> opc;
+            std::cin.ignore();
+            
+            if(opc == 's' || opc == 'S') {
+                continue;
+            }
+            else {
+                std::system("cls");
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+    std::vector<std::string> task;
+    std::string text;
+    char opc;
+    int indice;
+    
+    addTask(task, text, opc);
+    removeTask(task, text, opc, indice);
+
+    std::cout << "===== Lista de Tarefas =====" << std::endl;
+
+    for(auto i = 0; i < task.size(); i++) {
+        std::cout << (i + 1) << " - " << task[i] << std::endl;
+    }
+
+    return 0;
+}
